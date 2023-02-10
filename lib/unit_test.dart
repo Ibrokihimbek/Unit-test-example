556fe173dd956236f8557a53a84ebdc47e27ef92
Ibrokhimbek <ibrohim9902@gmail.com>
2023-02-10 13:41:23 +0500

    Unit test example

---
+++ lib/unit_test.dart
@@ -0,0 +1,58 @@
+class Utils{
+
+
+String baseUrl = "https://kun.uz";
+
+int myNumber = 100;
+
+
+bool isMaxNew(int amount, int maxAmount, double rate, bool isDollar) {
+    if (isDollar) {
+      return maxAmount < rate * amount;
+    } else {
+      return maxAmount < amount;
+    }
+  }
+
+
+
+bool stringEquals(String main, String sub) {
+    if (main.replaceAll(" ", "").length < sub.replaceAll(" ", "").length) {
+      return false;
+    } else {
+      if ((main
+              .replaceAll(" ", "")
+              .substring(0, sub.replaceAll(" ", "").length)
+              .toUpperCase()) ==
+          sub.replaceAll(" ", "").toUpperCase()) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+
+static String format(int k) {
+    if (k < 10) {
+      return "0$k";
+    } else {
+      return k.toString();
+    }
+  }
+
+
+
+String returnCardFormatText(String cardNumber) {
+  String part1 = cardNumber.substring(0, 4);
+  String part2 = cardNumber.substring(4, 8);
+  String part3 = cardNumber.substring(8, 12);
+  String part4 = cardNumber.substring(12, 16);
+  return "$part1 $part2 $part3 $part4";
+}
+
+
+
+
+
+}
---
+++ test/unit_test_test.dart
@@ -0,0 +1,66 @@
+import 'package:unit_test/unit_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group(
+    "UTILS CLASS PASSED",
+    () {
+      test(
+        "IS MAX NEW PASSED",
+        () {
+          expect(
+            Utils().isMaxNew(10, 20, 2.0, true),
+            false,
+          );
+        },
+      );
+
+      test(
+        "STRING EQUALS PASSED",
+        () {
+          expect(
+            Utils().stringEquals("salom", ""),
+            true,
+          );
+        },
+      );
+
+      test(
+        "FORMAT PASSED",
+        () {
+          expect(
+            Utils.format(9),
+            "0${9}",
+          );
+        },
+      );
+
+      test(
+        "CARD FORMAT TEXT PASSED",
+        () {
+          expect(
+            Utils().returnCardFormatText("1234567891011123"),
+            '${1234} ${5678} ${9101} ${1123}',
+          );
+        },
+      );
+      group(
+        "UTILS FIELDS PASSED",
+        () {
+          test(
+            'BASE URL PASSED',
+            () {
+              expect(Utils().baseUrl, isNotEmpty);
+            },
+          );
+          test(
+            "MY NUMBER PASSED",
+            () {
+              expect(Utils().myNumber, 100);
+            },
+          );
+        },
+      );
+    },
+  );
+}
